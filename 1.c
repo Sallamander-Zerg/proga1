7d2a98e925ba76502ccef16d235ec354f2c0750e
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2020-10-31 13:07:11 +0200

    Update 1.c

--- 1.c
+++ 1.c
@@ -11,20 +11,23 @@ int lm=0;
 int nm=0;
 int line[100][100];
 char ch;
-FILE *file = fopen("text.txt","r");
-line[0][0];
+int file = open( "text.txt", O_RDONLY, 0 );
+if(file <=0){
+printf("-1");
+}
+line[0][0]=0;
 while (read(file, &ch , sizeof(char))>0){
 nm++;
 if (ch == ('\n')) {
 line[lm][0]=nm;
 line[lm-1][1]=nm-line[lm-1][0];
 lm ++;
+printf("%d, %d, index = %d\n", line[lm-1][0],  line[lm-1][1], lm -1 );
 }
-
 }
 scanf("%d", &a);
-lseek(file,line[a][0],SEEK_SET);
-for(int i=0;i<line[a][1];i++){
+lseek(file,line[a-2][0],SEEK_SET);
+for(int i=0;i<line[a-2][1];i++){
 read(file, &ch , sizeof(char));
 printf("%c",ch);
 }
